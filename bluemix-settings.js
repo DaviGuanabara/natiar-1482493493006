46b6d941be45f00b01db0dc1d6404d6e165d25af
DaviGuanabara <daviguanabaraaragao@gmail.com>
2016-12-23 10:21:43 -0300

    Update bluemix-settings.js

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -23,8 +23,6 @@ var appEnv = cfenv.getAppEnv();
 var VCAP_APPLICATION = JSON.parse(process.env.VCAP_APPLICATION);
 var VCAP_SERVICES = JSON.parse(process.env.VCAP_SERVICES);
 
-var diretorio = 'https://github.com/DaviGuanabara/natiar-1482493493006/';
-
 var settings = module.exports = {
     uiPort: process.env.PORT || 1880,
     mqttReconnectTime: 15000,
@@ -46,12 +44,12 @@ var settings = module.exports = {
 
     // You can protect the user interface with a userid and password by using the following property
     // the password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
-    //httpAdminAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
+    httpAdminAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
     //httpAdminAuth: {user:"user",pass:"ee11cbb19052e40b07aac0ca060c23ee"},
     
     // Serve up the welcome page
     
-    httpStatic: path.join(__dirname,diretorio),
+    httpStatic: path.join(__dirname,"public"),
 
     functionGlobalContext: { },
 
